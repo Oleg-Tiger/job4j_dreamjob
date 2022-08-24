35c5b22c9125ddb0831415806aff482de0eb5e0d
Oleg-Tiger <free_person@inbox.ru>
2022-08-24 21:12:37 +0300

    5. Создания и редактирования кандидатов. [#504858]

--- src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -1,20 +1,36 @@
 package ru.job4j.dreamjob.store;
 
 import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.Post;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CandidateStore {
 
     private static final CandidateStore INST = new CandidateStore();
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    private static final AtomicInteger ID = new AtomicInteger(3);
 
     private CandidateStore() {
         candidates.put(1, new Candidate(1, "Junior Java Developer"));
         candidates.put(2, new Candidate(2, "Middle Java Developer"));
         candidates.put(3, new Candidate(3, "Senior Java Developer"));
+    }
+
+    public Candidate add(Candidate candidate) {
+        candidate.setId(ID.incrementAndGet());
+        return candidates.put(candidate.getId(), candidate);
+    }
+
+    public Candidate findById(int id) {
+        return candidates.get(id);
+    }
+
+    public Candidate update(Candidate candidate) {
+        return candidates.replace(candidate.getId(), candidate);
     }
 
     public static CandidateStore instOf() {
@@ -24,5 +40,4 @@ public class CandidateStore {
     public Collection<Candidate> findAll() {
         return candidates.values();
     }
-
 }
